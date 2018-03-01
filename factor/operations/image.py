13b049ed88f2f0d5e82952460817da54c936aab3
David Rafferty <drafferty@hs.uni-hamburg.de>
2018-03-01 10:28:01 +0100

    Fix call

--- factor/operations/image.py
+++ factor/operations/image.py
@@ -42,10 +42,10 @@ class Image(Operation):
         # Save output mapfiles for later use
         in_map = DataMap.load(os.path.join(self.pipeline_mapfile_dir,
                                            'image-MFS-image.fits.mapfile'))
-        self.direction.add_output_image_filename(in_map[0].file)
+        self.direction.store_output_image_filename(in_map[0].file)
         in_map = DataMap.load(os.path.join(self.pipeline_mapfile_dir,
                                            'image-sources.txt.mapfile'))
-        self.direction.add_output_skymodel_filename(in_map[0].file)
+        self.direction.store_output_skymodel_filename(in_map[0].file)
 
         # Create sym links to images
         sol_map = DataMap.load(self.field.h5parm_mapfile)
