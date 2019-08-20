Fix off-by-one bug

factor/lib/observation.py
@@ -70,7 +70,7 @@ class Observation(object):
         else:
             self.goesto_endofms = True
         self.timepersample = tab.getcell('EXPOSURE', 0)
-        self.numsamples = int(np.ceil((self.endtime - self.starttime) / self.timepersample)) + 1
+        self.numsamples = int(np.ceil((self.endtime - self.starttime) / self.timepersample))
         tab.close()
 
         # Get frequency info