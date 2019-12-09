f96142bb983f3de8fbb867d3c9d3cc3c87e79bea
David Rafferty <drafferty@hs.uni-hamburg.de>
2019-12-09 11:28:00 +0100

    Fix var

--- factor/operations/image.py
+++ factor/operations/image.py
@@ -75,7 +75,7 @@ class Image(Operation):
                             'region_file': self.direction.region_file,
                             'use_beam': self.direction.use_beam,
                             'wsclean_niter': self.direction.wsclean_niter,
-                            'robust': self.direction.robust,
+                            'robust': robust,
                             'wsclean_image_padding': self.direction.wsclean_image_padding,
                             'cellsize_deg': self.direction.cellsize_deg,
                             'min_uv_lambda': self.direction.min_uv_lambda,
