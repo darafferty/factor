eeb43ce5d72b169f7adb4785690aaf10d014fa44
David Rafferty <drafferty@hs.uni-hamburg.de>
2020-02-25 12:41:58 +0100

    Update defaults

--- factor/lib/sector.py
+++ factor/lib/sector.py
@@ -47,7 +47,8 @@ class Sector(object):
         self.field = field
         self.vertices_file = os.path.join(field.working_dir, 'regions', '{}_vertices.pkl'.format(self.name))
         self.region_file = "'[]'"
-        self.I_image_file = None  # set by the Image operation
+        self.I_image_file_true_sky = None  # set by the Image operation
+        self.I_image_file_apparent_sky = None  # set by the Image operation
         self.image_skymodel_file_apparent_sky = None  # set by the Image operation
         self.image_skymodel_file_true_sky = None  # set by the Image operation
         self.is_outlier = False
